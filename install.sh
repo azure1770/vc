5e4065087dc3aef469a4e2b816d2c982767328f2
azure1770 <118550123+azure1770@users.noreply.github.com>
2022-12-24 17:30:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
 #/bin/bash
+
+#Check Veracrypt
+if [ ! -f /bin/veracrypt ]
+then
+    wget `curl https://veracrypt.fr/en/Downloads.html | grep "setup.tar.bz2" | grep -iv -e "sig" -e "x86" -e "freebsd" | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' -e 's/&#43;/+/'`
+    unp veracrypt*setup.tar.bz2
+    ./veracrypt*setup-console-x64
+    sudo rm veracrypt* -R
+fi
+
+#Check KeepassXC
+if [[ `dpkg -l | grep -E '^ii' | grep keepassxc | cut -d" " -f 3` == "keepassxc" ]]
+then
+    :
+else
+    sudo add-apt-repository ppa:phoerious/keepassxc -y
+    sudo apt-get update
+    sudo apt install keepassxc -y
+fi
+
 sudo cp ./vc /bin
 sudo cp ./vcvars /bin
 sudo cp ./vcsec /bin
-chmod +x /bin/vc
+sudo chmod +x /bin/vc
