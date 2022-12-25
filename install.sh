4aaedba5f59122207f9776d6d2e65bab4fd594e0
azure1770 <118550123+azure1770@users.noreply.github.com>
2022-12-25 13:14:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 
+
 #Check Veracrypt
 if [ ! -f /bin/veracrypt ]
 then
+    apt-get install unp -y
     wget `curl https://veracrypt.fr/en/Downloads.html | grep "setup.tar.bz2" | grep -iv -e "sig" -e "x86" -e "freebsd" | grep -o '<a .*href=.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' -e 's/&#43;/+/'`
     unp veracrypt*setup.tar.bz2
     ./veracrypt*setup-console-x64
@@ -10,7 +12,7 @@ then
 fi
 
 #Check KeepassXC
-if [[ `dpkg -l | grep -E '^ii' | grep keepassxc | cut -d" " -f 3` == "keepassxc" ]]
+if [ `dpkg -l | grep -E '^ii' | grep keepassxc | cut -d" " -f 3` = 'keepassxc' ]
 then
     :
 else
@@ -23,3 +25,33 @@ sudo cp ./vc /bin
 sudo cp ./vcvars /bin
 sudo cp ./vcsec /bin
 sudo chmod +x /bin/vc
+
+#Create first Veracrypt Container
+echo ""
+
+while true
+do
+read -r -n 1 -p "Would you create a Veracrypt Container (Y|n)? " cvcc && printf "\n"
+case "$cvcc" in
+    y|Y|"")
+    veracrypt -t -c
+    break
+    ;;
+    n|N)
+    break
+    ;;
+    *)
+      echo " Invalid input..."
+    ;;
+esac
+done
+
+sudo nano /bin/vcvars
+
+clear
+echo ""
+echo ""
+echo -e "\e[92m******************\e[39m"
+echo -e "\e[92m* Setup Finished *\e[39m"
+echo -e "\e[92m******************\e[39m"
+echo ""
